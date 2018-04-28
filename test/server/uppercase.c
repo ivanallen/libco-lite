43518aabd724e5a4badc0d8bc3def7da25f37a27
kingchin1218 <kingchin1218@126.com>
2018-04-29 01:07:36 +0800

    add LISENCE

--- test/server/uppercase.c
+++ test/server/uppercase.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018, kingchin1218@126.com 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,30 +41,39 @@
 int handle(int fd);
 size_t upper(char buf[], int n);
 
-int handle(int fd) {
-	char buf[MAXSIZE];
-	size_t n = read(fd, buf, MAXSIZE);
-	buf[n] = '\n';
-	
-	printf("serv:msg from client[%s]. len = %d\n", buf, n);
+/**
+*returned -1 means peer socket closed,otherwise means the len of buf
+*/
+int get_line(int fd, char *buf, size_t len) {
+        size_t n = 0;
+        int curr = 0;
+        do {
+                n = read(fd, buf + curr, len);
+                curr += n;
+        } while(n != 0 && buf[curr - 1]  != '\n');
 
-	if (0 == n) {
-		return -1;
-	}
-	
-	printf("serv:will be upper\n");
-	
-	n = upper(buf, n);
-	
-	printf("serv:will be send msg to client[%s], len = %d\n", buf, n);
-	
-	n = write(fd, buf, n);
-	
-	printf("serv: send msg to client[%s]. len = %d\n", buf, n);
-	
-	return n;
+        if (n == 0) {
+                return -1;
+        }
+
+        return curr;
 }
 
+int handle(int fd) {
+        char buf[MAXSIZE];
+
+        size_t n = get_line(fd, buf, MAXSIZE);
+        buf[n] = '\n';
+
+        if (0 == n) {
+                return -1;
+        }
+
+        n = upper(buf, n);
+        write(fd, buf, n);
+
+        return n;
+}
 
 size_t upper(char buf[], int n) {
 	int i = 0;
@@ -51,7 +87,7 @@ size_t upper(char buf[], int n) {
 		++i;
 	} 
 
-	return i;
+	return i+1;
 }
 
 int main (int argc, char **argv) {
